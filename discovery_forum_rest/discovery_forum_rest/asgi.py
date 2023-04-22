b2f937e4ca77272b2dca4efee570a1598cef50d5
Ger-ych <viking-e@bk.ru>
2023-04-22 21:10:18 +0500

    first commit

---
+++ discovery_forum_rest/discovery_forum_rest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for discovery_forum_rest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discovery_forum_rest.settings')
+
+application = get_asgi_application()
