14e8e8c5da4368a02ad46c942627ae40a12757a3
Ger-ych <viking-e@bk.ru>
2023-04-24 23:51:53 +0500

    Feat(questions-models): Create question models

--- discovery_forum_rest/discovery_forum_rest/apps/questions/models.py
+++ discovery_forum_rest/discovery_forum_rest/apps/questions/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+# question category model
+class QuestionCategory(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Название категории")
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория вопросов'
+        verbose_name_plural = 'Категории вопросов'
+
+# question model
+class Question(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        related_name='questions',
+        verbose_name='Пользователь',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    heading = models.CharField(verbose_name="Заголовок", max_length=255)
+    text = models.TextField(verbose_name="Текст")
+    category = models.ForeignKey(
+        QuestionCategory,
+        related_name='questions',
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    keywords = models.TextField(verbose_name="Ключевые слова")
+    following_users = models.ManyToManyField(
+        get_user_model(),
+        related_name="followed_questions",
+    )
+    
+    def __str__(self):
+        return self.heading
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+# question comment model
+class QuestionComment(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        related_name='question_comments',
+        verbose_name='Пользователь',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    question = models.ForeignKey(
+        Question,
+        related_name='comments',
+        verbose_name='Пользователь',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    text = models.CharField(verbose_name="Текст", max_length=512)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Комментарий к вопросу'
+        verbose_name_plural = 'Комментарии к вопросам'
